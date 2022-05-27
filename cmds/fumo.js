6f4400993b36a811f717315bd009095787c2b428
Kuukee <lf4122@gmail.com>
2022-05-27 03:43:38 +0200

    Added cooldown so you can't spam the command

--- cmds/fumo.js
+++ cmds/fumo.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 //const requireDirectory = require('require-directory');
 const config = require('../config.json');
 const path = require("path");
+const talkedRecently = new Set();
 
 const info = {
 	name: 'fumo',
@@ -12,52 +13,71 @@ const info = {
 };
 
 function exec(args, vars) {
-
-	function searchFumos(search) {
-		var items = [];
-		var file = fs.readdirSync(path.resolve(__dirname, "../fumoFolder/"));
-		console.log(file);
-		var files = file.map(file => file.toLowerCase());
-		for(var file of files) {
-		  if(search.every(v => file.includes(v))) {
-			  items.push(file);
-		   }}
-		return items;
-	}
-
-	var filter = args.join(' '); //if the arguments are an array by default
-	var search = filter.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').split(' '); //Cleans up the filter removing special characters and puts everything in lowercase
-	var items = searchFumos(search); //calls function to get all the fumos filtered
-	console.log(items); //Just for debugging
-	if (items.length > 0) {
-
-		var fumoSend = items[Math.floor(Math.random() * items.length)]; //gets random file from that list
-		console.log(fumoSend);
-		var finalSend = "./fumoFolder/" + fumoSend;
-		console.log(finalSend);
-
+	if ((talkedRecently.has(vars.msg.author.id)) && (vars.msg.author.id !== vars.config.owner)) {
 		var embed = new discord.MessageEmbed()
-		.setColor("#07B05F")
-		.setTitle(fumoSend)
-		.setDescription("Here ya go!" + ' <@!' + vars.msg.author + '>')
-		.setImage(url='attachment://fumoSend')
+		.setColor("#f03607")
+		.setTitle("Shut up")
+		.setDescription("Bro calm tf down" + ' <@!' + vars.msg.author + '>')
 		.setTimestamp();
-		vars.msg.reply({embeds:[embed], files:[finalSend]});
-		// vars.msg.reply("Here you go " + '<@!' + msg.author + '>', {files: [{attachment: config.fumoFolder + FumoSend}]});			Archived
-		vars.msg.react('✅');
+		vars.msg.reply({embeds:[embed]})
+			.then(msg => {
+				setTimeout(() => msg.delete(), 3000)
+			});
 	} else {
-		searchFumos();
-		var consolationFumo = items[Math.floor(Math.random() * items.length)];
-		console.log("epic fail");
+		//Puts user in cooldown
+		talkedRecently.add(vars.msg.author.id);
+		setTimeout(() => {
+			// Removes the user from the set after a minute
+			talkedRecently.delete(vars.msg.author.id);
+		}, 8000);
 
-		var embed = new discord.MessageEmbed()
-		.setTitle('Sorry I couldn\'t find a file with that search')
-		.setColor("#ff3c33")
-		.setDescription('Here\'s a consolation fumo :3')
-		.setImage([{attachment: config.fumoFolder + consolationFumo}])
-		.setTimestamp();
-		vars.msg.reply({embeds:[embed]});
-		// vars.msg.reply("No fumo found, EPIC FAIL!||, only send one word after the command to make it easier||");			Archived
+		function searchFumos(search) {
+			var items = [];
+			var file = fs.readdirSync(path.resolve(__dirname, "../fumoFolder/"));
+			var files = file.map(file => file.toLowerCase());
+			for(var file of files) {
+			if(search.every(v => file.includes(v))) {
+				items.push(file);
+			}}
+			return items;
+		}
+
+		var filter = args.join(' '); //if the arguments are an array by default
+		var search = filter.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').split(' '); //Cleans up the filter removing special characters and puts everything in lowercase
+		var items = searchFumos(search); //calls function to get all the fumos filtered
+		//console.log(items); //Just for debugging
+		if (items.length > 0) {
+
+			var fumoSend = items[Math.floor(Math.random() * items.length)]; //gets random file from that list
+			//console.log(fumoSend);
+			var finalSend = "./fumoFolder/" + fumoSend;
+			//console.log(finalSend);
+
+			var embed = new discord.MessageEmbed()
+			.setColor("#07B05F")
+			.setTitle(fumoSend)
+			.setDescription("Here ya go!" + ' <@!' + vars.msg.author + '>')
+			.setTimestamp();
+			vars.msg.reply({embeds:[embed], files:[finalSend]});
+			// vars.msg.reply("Here you go " + '<@!' + msg.author + '>', {files: [{attachment: config.fumoFolder + FumoSend}]});			Archived
+			vars.msg.react('✅');
+		} else {
+			var embed = new discord.MessageEmbed()
+			.setTitle("Sorry I couldn't find a file with that search")
+			.setDescription("Don't worry you aren't in cooldown.")
+			.setColor("#ff3c33")
+			.setTimestamp();
+			vars.msg.reply({embeds:[embed]})
+			.then(msg => {
+				setTimeout(() => msg.delete(), 3000)
+			});
+			talkedRecently.delete(vars.msg.author.id);
+			vars.msg.react('❎');
+			// vars.msg.reply("No fumo found, EPIC FAIL!||, only send one word after the command to make it easier||");			Archived
+
+
+		} 			
+
 }}
 
 exports.info = info;
