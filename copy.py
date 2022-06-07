2c4ce8b3684c03df871244c8523b03b4cced3bfe
Kuukee <lf4122@gmail.com>
2022-06-07 19:33:06 +0200

    made it rename all files to lowercase. reason: linux

--- copy.py
+++ copy.py
@@ -14,8 +14,10 @@ for folder, subfolders, files in os.walk(src):
     for file in files:
         src=os.path.join(folder, file)
         if file in os.listdir(dst):
-            #print(file+" exists.")             remove the comment to enable logging which files are or arent there
+            os.rename(dst+"\\"+file, dst+"\\"+file.lower())
+            #print(file+" exists.")             #remove the comment to enable logging which files are or arent there
             pass
         else:
-            #print(file+' does not exist.')     remove the comment to enable logging which files are or arent there
+            #print(file+' does not exist.')     #remove the comment to enable logging which files are or arent there
             shutil.copy2(src, dst)
+            os.rename(dst+"\\"+file, dst+"\\"+file.lower()) #renames all files to lowercase
